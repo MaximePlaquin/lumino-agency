46372bb144199acfc91babd55d598c8f200270d9
maximeplaquin <plaquin.maxime@gmail.com>
2025-05-13 11:56:50 +0200

    Integration GA4

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,22 +1,22 @@
-import { Inter } from "next/font/google"
-import "./globals.css"
-import { Header } from "@/components/layout/header"
-import { Footer } from "@/components/layout/footer"
-import { Analytics } from "@/components/analytics"
-import { getSiteMetadata } from "@/lib/data/metadata"
-import { Suspense } from "react"
-import { Toaster } from "@/components/ui/toaster"
-
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Header } from "@/components/layout/header";
+import { Footer } from "@/components/layout/footer";
+import { Analytics } from "@/components/analytics";
+import { getSiteMetadata } from "@/lib/data/metadata";
+import { Suspense } from "react";
+import { Toaster } from "@/components/ui/toaster";
+import Script from "next/script";
 
 // Police principale
 const inter = Inter({
   subsets: ["latin"],
   display: "swap",
-})
+});
 
 export function generateMetadata() {
   // Récupération des métadonnées du site (simulation d'appel API)
-  const metadata = getSiteMetadata()
+  const metadata = getSiteMetadata();
 
   return {
     title: {
@@ -34,7 +34,7 @@ export function generateMetadata() {
       locale: "fr_FR",
       type: "website",
     },
-  }
+  };
 }
 
 export default function RootLayout({ children }) {
@@ -44,11 +44,24 @@ export default function RootLayout({ children }) {
         <>
           <Header />
           <Suspense fallback={<div>Chargement...</div>}>{children}</Suspense>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=G-MH1FSV0RZ5`}
+            strategy="afterInteractive"
+          />
+
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-MH1FSV0RZ5');
+`}
+          </Script>
           <Toaster />
           <Footer />
           <Analytics />
         </>
       </body>
     </html>
-  )
+  );
 }
